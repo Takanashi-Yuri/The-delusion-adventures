62d0af261c77d3fad7f3a128ab23aba6ec1d5507
Takanashi-Yuri <crystalfantasy0000@proton.me>
2023-07-10 03:53:22 +0200

    Added base files for libGDX

---
+++ desktop/src/com/magic/delusion/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.magic.delusion;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.magic.delusion.The-delusion-adventures;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("The delusion adventures");
+		new Lwjgl3Application(new The-delusion-adventures(), config);
+	}
+}
